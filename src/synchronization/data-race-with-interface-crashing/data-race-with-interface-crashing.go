8c76446f0023e587060641374116fde7c991814f
Picci <enrico.piccinin@gmail.com>
2022-11-16 16:19:07 +0100

    data races with interfaces  generate runtime error

--- src/synchronization/data-race-with-interface-crashing/data-race-with-interface-crashing.go
+++ src/synchronization/data-race-with-interface-crashing/data-race-with-interface-crashing.go
@@ -5,8 +5,6 @@ import (
 	"os"
 )
 
-var sum int
-
 // In this example we define an interface type Doer and a shared global variable, sharedInterface, of type Doer.
 // The shared variable is assigned values by 2 different goroutines concurrenlty.
 // The 2 goroutines assign values of different types to sharedInterface variable, one of type Human and one of type Computer
@@ -22,30 +20,26 @@ const human = "Human"
 const computer = "Computer"
 
 type Human struct {
-	Name     string
-	Siblings []int
+	Name           string
+	MyLuckyNumbers []int
 }
 
-func (d *Human) DoStuff() bool {
-	// do some processing with the Parents map
-	// this is a safe operation as long as the Parents map is actually a slice
+func (d Human) DoStuff() bool {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in f", r)
-			fmt.Println("valueInSlice")
-			for _, v := range d.Siblings {
+			fmt.Println("These are my corrupted lucky numbers")
+			for _, v := range d.MyLuckyNumbers {
 				fmt.Println(">>>>>>>******", v)
 			}
 		}
 	}()
-	// for _, v := range d.Siblings {
-	// 	sum = sum + v
-	// }
-	if d.Siblings[10] != 10 {
-		for _, v := range d.Siblings {
-			sum = sum + v
-		}
-		fmt.Printf("I am the Human doing stuff as a %v\n", d.Name)
+
+	// do something with the MyLuckyNumbers
+	// if the receiver d happens to be corrupted (which is possible since the share variable sharedInterface is not protected against data races)
+	// then there may be a runtime error
+	if d.MyLuckyNumbers[5] != 5 {
+		fmt.Println("My sixth lucky number should be 5")
 		return false
 	}
 
@@ -58,11 +52,7 @@ type Machine struct {
 }
 
 func (d Machine) DoStuff() bool {
-	if d.Name != computer {
-		fmt.Printf("I am the Machine doing stuff as a %v\n", d.Name)
-		// return false
-	}
-
+	// do nothing and return always true
 	return true
 }
 
@@ -72,22 +62,20 @@ func main() {
 	// If it returns false it means that the interface value is corrupted and the corruption derives that the assignement to the shared variable
 	// sharedInterface (the assignement is a write operation) is run concurrently and is not protected by a synchronization mechanism
 	doStuffManyTimes := func(d Doer) {
-		for i := 0; i < 1000000; i++ {
+		i := 0
+		for {
 			// this is the assignement operation (write operation) which is not protected and therefore can cause data corruption
 			sharedInterface = d
 			if !sharedInterface.DoStuff() {
 				fmt.Printf("Something strange happened in iteration %v\n", i)
 				os.Exit(1)
 			}
+			i++
 		}
-		fmt.Println("DONE", sum)
 	}
 
-	s := make([]int, 100)
-	for i := 0; i < 100; i++ {
-		s[i] = i
-	}
-	d_1 := Human{human, s}
+	luckyNumbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	d_1 := Human{human, luckyNumbers}
 	go doStuffManyTimes(&d_1)
 
 	d_2 := Machine{computer, "M24"}
@@ -96,87 +84,3 @@ func main() {
 	// stop forever used here just for the laziness of not adding wait groups
 	select {}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-// )
-
-// // In this example we define an interface type Doer and a shared global variable, sharedInterface, of type Doer.
-// // The shared variable is assigned values by 2 different goroutines concurrenlty.
-// // The 2 goroutines assign values of different types to sharedInterface variable, one of type Human and one of type Computer
-// // So, in a concurrent way, we assign values of different concrete types to a variable of type interface.
-// // Since the assignement is not protected by synchronization mechanisms, we can have data corruption that leads to wierd behavior:
-// // a concrete value of a certain type runs the method implementation of a different type; for instance a value of type Human executes the method DoStuff
-// // implemented by the type Computer, or viceversa
-// type Doer interface {
-// 	DoStuff() bool
-// }
-
-// var sharedInterface Doer
-
-// const human = "Human>>>>>>>>>>>>>"
-// const computer = "Computer================="
-
-// const age = 20000
-
-// type Human struct {
-// 	Name  string
-// 	Age   int
-// 	MyMap map[int]int
-// }
-
-// func (d Human) DoStuff() bool {
-// 	for k, v := range d.MyMap {
-// 		fmt.Println(k, v)
-// 	}
-// 	if d.Age != age {
-// 		fmt.Printf("I am the Human doing stuff as a %v\n", d.Age)
-// 		// return false
-// 	}
-
-// 	return true
-// }
-
-// type Machine struct {
-// 	Name  string
-// 	Model string
-// }
-
-// func (d Machine) DoStuff() bool {
-// 	// if d.Name != computer {
-// 	// 	fmt.Printf("I am the Machine doing stuff as a %v\n", d.Name)
-// 	// 	return false
-// 	// }
-
-// 	return true
-// }
-
-// func main() {
-
-// 	// this is a function that calls DoStuff many times and checks whether DoStuff returns false.
-// 	// If it returns false it means that the interface value is corrupted and the corruption derives that the assignement to the shared variable
-// 	// sharedInterface (the assignement is a write operation) is run concurrently and is not protected by a synchronization mechanism
-// 	doStuffManyTimes := func(d Doer) {
-// 		for i := 0; i < 1000000; i++ {
-// 			// this is the assignement operation (write operation) which is not protected and therefore can cause data corruption
-// 			sharedInterface = d
-// 			if !sharedInterface.DoStuff() {
-// 				fmt.Printf("Something strange happened in iteration %v\n", i)
-// 				os.Exit(1)
-// 			}
-// 		}
-// 		fmt.Println("DONE")
-// 	}
-
-// 	d_1 := Human{human, age, make(map[int]int)}
-// 	go doStuffManyTimes(d_1)
-
-// 	d_2 := Machine{computer, "M25"}
-// 	go doStuffManyTimes(d_2)
-
-// 	// stop forever used here just for the laziness of not adding wait groups
-// 	select {}
-// }
